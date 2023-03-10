13f92697d188f865d31fc0eb2bfa6b947a919362
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-10 14:26:18 +0200

    Feature component

---
+++ src/components/feature/feature.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './feature.module.scss';
+
+function Feature({ icon, description }) {
+    return (
+        <div className={`${styles.feature} col col-3 bg-light border border-dark rounded-circle`}>
+            {icon}
+            <p className={styles['feature-description']}>{description}</p>
+        </div>
+    );
+}
+
+export default Feature;
