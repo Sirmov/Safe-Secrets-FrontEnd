c8c65d412bcef6e7af6204abc0f9f694e329674b
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-12 19:17:21 +0200

    Configured directory aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+    root: './src',
     plugins: [react()],
+    resolve: {
+        alias: [
+            { find: '@', replacement: path.resolve(__dirname, './src') },
+            { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
+            { find: '@components', replacement: path.resolve(__dirname, './src/components') },
+            { find: '@hooks', replacement: path.resolve(__dirname, './src/hooks') },
+            { find: '@pages', replacement: path.resolve(__dirname, './src/pages') },
+            { find: '@services', replacement: path.resolve(__dirname, './src/services') },
+        ],
+    },
 });
