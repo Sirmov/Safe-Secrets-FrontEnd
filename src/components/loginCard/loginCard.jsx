056e05d9124203e01205b119b5e433808cbb8154
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-13 13:30:53 +0200

    Added data validation

--- src/components/loginCard/loginCard.jsx
+++ src/components/loginCard/loginCard.jsx
@@ -1,41 +1,151 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { IconBrandGithub, IconBrandTwitter, IconEye } from '@tabler/icons-react';
+import { IconEye, IconEyeOff } from '@tabler/icons-react';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
 
 function LoginCard() {
+    const allowedData = ['email', 'password'];
+
+    const [revealPassword, setRevealPassword] = useState(false);
+    const [data, setData] = useState(allowedData.reduce((acc, curr) => Object.assign(acc, { [curr]: '' }), {}));
+    const [errors, setErrors] = useState(allowedData.reduce((acc, curr) => Object.assign(acc, { [curr]: '' }), {}));
+
+    function handleLogin(event) {
+        event.preventDefault();
+
+        event.preventDefault();
+
+        let errors = Object.entries(data).reduce((acc, [k, v]) => Object.assign(acc, { [k]: validateData(k, v) }), {});
+        let isValid = !Object.values(errors).some((e) => e.length > 0);
+
+        if (!isValid) {
+            setErrors(errors);
+        } else {
+            // ToDo send request
+        }
+    }
+
+    function handleDataChange(event) {
+        const dataKey = event.currentTarget.name;
+        const dataValue = event.currentTarget.value;
+
+        setData({ ...data, [dataKey]: dataValue });
+    }
+
+    function handleDataValidation(event) {
+        const dataKey = event.currentTarget.name;
+        const dataValue = event.currentTarget.value;
+        validateData(dataKey, dataValue);
+    }
+
+    function validateData(dataKey, dataValue) {
+        let validationFunction = () => '';
+
+        if (dataKey === 'email') {
+            validationFunction = validateEmail;
+        } else if (dataKey === 'password') {
+            validationFunction = validatePassword;
+        }
+
+        const error = validationFunction(dataValue);
+        setErrors({ ...errors, [dataKey]: error });
+
+        return error;
+    }
+
+    function validateEmail(email) {
+        const errors = [];
+
+        if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email) === false) {
+            errors.push('Email is invalid.');
+        }
+
+        return errors.join(' ');
+    }
+
+    function validatePassword(password) {
+        const errors = [];
+
+        if (password.length < 8 || password.length > 32) {
+            errors.push('Password length should be between 8 and 32 characters long.');
+        } else if (/\d/.test(password) === false) {
+            errors.push('Password should include at least one number.');
+        } else if (/[A-Z]/.test(password) === false) {
+            errors.push('Password should include at least one uppercase character.');
+        }
+
+        return errors.join(' ');
+    }
+
+    function handleRevealPassword() {
+        setRevealPassword((revealPassword) => !revealPassword);
+    }
+
     return (
         <article className="card card-md">
             <div className="card-body">
                 <h2 className="h2 text-center mb-4">Login to your account</h2>
-                <form action="./" method="get" autoComplete="off" noValidate>
+                <form onSubmit={handleLogin} autoComplete="off" noValidate>
                     <div className="mb-3">
-                        <label className="form-label">Email address</label>
-                        <input type="email" className="form-control" placeholder="your@email.com" autoComplete="off" />
+                        <label htmlFor="login-email" className="form-label">
+                            Email address
+                        </label>
+                        <input
+                            id="login-email"
+                            name="email"
+                            type="email"
+                            onChange={handleDataChange}
+                            onBlur={handleDataValidation}
+                            className={classNames({
+                                'form-control': true,
+                                'is-invalid': errors.email,
+                            })}
+                            value={data.email}
+                            placeholder="your@email.com"
+                            autoComplete="off"
+                        />
+                        <div className="invalid-feedback">{errors.email}</div>
                     </div>
                     <div className="mb-2">
-                        <label className="form-label">
+                        <label htmlFor="login-password" className="form-label">
                             Password
                             <span className="form-label-description">
-                                <a href="./forgot-password.html">Forgot password?</a>
+                                <Link to="/not-implemented">Forgot password?</Link>
                             </span>
                         </label>
-                        <div className="input-group input-group-flat">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="your_secret_password"
-                                autoComplete="off"
-                            />
-                            <span className="input-group-text">
+                        <div className="row g-2">
+                            <div className="col">
+                                <input
+                                    id="login-password"
+                                    name="password"
+                                    type={revealPassword ? 'text' : 'password'}
+                                    onChange={handleDataChange}
+                                    onBlur={handleDataValidation}
+                                    className={classNames({
+                                        'form-control': true,
+                                        'is-invalid': errors.password,
+                                    })}
+                                    value={data.password}
+                                    placeholder="your_secret_password"
+                                    autoComplete="off"
+                                />
+                                <div className="invalid-feedback">{errors.password}</div>
+                            </div>
+                            <div className="col-auto">
                                 <a
-                                    href="#"
-                                    className="link-secondary"
+                                    onClick={handleRevealPassword}
+                                    className="btn btn-icon"
                                     data-bs-toggle="tooltip"
                                     aria-label="Show password"
                                     data-bs-original-title="Show password">
-                                    <IconEye className="icon" color="currentColor" stroke={2} size={24} />
+                                    {revealPassword ? (
+                                        <IconEyeOff className="icon" color="currentColor" stroke={2} size={24} />
+                                    ) : (
+                                        <IconEye className="icon" color="currentColor" stroke={2} size={24} />
+                                    )}
                                 </a>
-                            </span>
+                            </div>
                         </div>
                     </div>
                     <div className="mb-2">
@@ -50,23 +160,6 @@ function LoginCard() {
                         </button>
                     </div>
                 </form>
-            </div>
-            <div className="hr-text">or</div>
-            <div className="card-body">
-                <div className="row">
-                    <div className="col">
-                        <a href="#" className="btn w-100">
-                            <IconBrandGithub className="icon" color="currentColor" stroke={2} size={24} />
-                            Login with Github
-                        </a>
-                    </div>
-                    <div className="col">
-                        <a href="#" className="btn w-100">
-                            <IconBrandTwitter className="icon" color="#1da1f2" stroke={2} size={24} />
-                            Login with Twitter
-                        </a>
-                    </div>
-                </div>
             </div>
         </article>
     );
--- src/components/signUpCard/signUpCard.jsx
+++ src/components/signUpCard/signUpCard.jsx
@@ -1,54 +1,220 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { IconEye } from '@tabler/icons-react';
+import { IconEye, IconEyeOff } from '@tabler/icons-react';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
 
 function SignUpCard() {
+    const allowedData = ['username', 'email', 'password', 'terms'];
+
+    const [revealPassword, setRevealPassword] = useState(false);
+    const [data, setData] = useState(allowedData.reduce((acc, curr) => Object.assign(acc, { [curr]: '' }), {}));
+    const [errors, setErrors] = useState(allowedData.reduce((acc, curr) => Object.assign(acc, { [curr]: '' }), {}));
+
+    function handleRegister(event) {
+        event.preventDefault();
+
+        let errors = Object.entries(data).reduce((acc, [k, v]) => Object.assign(acc, { [k]: validateData(k, v) }), {});
+        let isValid = !Object.values(errors).some((e) => e.length > 0);
+
+        if (!isValid) {
+            setErrors(errors);
+        } else {
+            // ToDo send request
+        }
+    }
+
+    function handleDataChange(event) {
+        const dataKey = event.currentTarget.name;
+        const dataValue = event.currentTarget.value;
+
+        setData({ ...data, [dataKey]: dataValue });
+    }
+
+    function handleDataValidation(event) {
+        const dataKey = event.currentTarget.name;
+        const dataValue = event.currentTarget.value;
+        validateData(dataKey, dataValue);
+    }
+
+    function validateData(dataKey, dataValue) {
+        let validationFunction = () => '';
+
+        if (dataKey === 'email') {
+            validationFunction = validateEmail;
+        } else if (dataKey === 'password') {
+            validationFunction = validatePassword;
+        } else if (dataKey === 'username') {
+            validationFunction = validateUsername;
+        } else if (dataKey === 'terms') {
+            validationFunction = validateTerms;
+        }
+
+        const error = validationFunction(dataValue);
+        setErrors({ ...errors, [dataKey]: error });
+
+        return error;
+    }
+
+    function validateUsername(username) {
+        const errors = [];
+
+        if (username.length < 3 || username.length > 16) {
+            errors.push('Username length should be between 3 and 16 characters.');
+        } else if (/^[a-zA-Z0-9]+$/.test(username) === false) {
+            errors.push('Username should not contains special characters.');
+        }
+
+        return errors.join(' ');
+    }
+
+    function validateEmail(email) {
+        const errors = [];
+
+        if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email) === false) {
+            errors.push('Email is invalid.');
+        }
+
+        return errors.join(' ');
+    }
+
+    function validatePassword(password) {
+        const errors = [];
+
+        if (password.length < 8 || password.length > 32) {
+            errors.push('Password length should be between 8 and 32 characters long.');
+        } else if (/\d/.test(password) === false) {
+            errors.push('Password should include at least one number.');
+        } else if (/[A-Z]/.test(password) === false) {
+            errors.push('Password should include at least one uppercase character.');
+        }
+
+        return errors.join(' ');
+    }
+
+    function validateTerms(terms) {
+        if (stringToBoolean(terms) == false) {
+            return 'You should accept the terms and policy.';
+        }
+
+        return '';
+    }
+
+    function stringToBoolean(string) {
+        if (string === false || String(string).toLowerCase() === 'false') {
+            return false;
+        } else if (string === true || String(string).toLowerCase() === 'true') {
+            return true;
+        }
+
+        return Boolean(string);
+    }
+
+    function handleRevealPassword() {
+        setRevealPassword((revealPassword) => !revealPassword);
+    }
+
     return (
         <article className="card card-md">
             <div className="card-body">
                 <h2 className="h2 text-center mb-4">Create new account</h2>
-                <form action="./" method="get" autoComplete="off" noValidate>
+                <form onSubmit={handleRegister} autoComplete="off" noValidate>
                     <div className="mb-3">
-                        <label className="form-label">Name</label>
-                        <input type="text" className="form-control" placeholder="Enter name" wfd-id="id0" />
+                        <label htmlFor="register-username" className="form-label">
+                            Name
+                        </label>
+                        <input
+                            id="register-username"
+                            name="username"
+                            type="text"
+                            onChange={handleDataChange}
+                            onBlur={handleDataValidation}
+                            className={classNames({
+                                'form-control': true,
+                                'is-invalid': errors.username,
+                            })}
+                            value={data.username}
+                            placeholder="Enter username"
+                        />
+                        <div className="invalid-feedback">{errors.username}</div>
                     </div>
                     <div className="mb-3">
-                        <label className="form-label">Email address</label>
-                        <input type="email" className="form-control" placeholder="Enter email" wfd-id="id1" />
+                        <label htmlFor="register-email" className="form-label">
+                            Email address
+                        </label>
+                        <input
+                            id="register-email"
+                            name="email"
+                            type="email"
+                            onChange={handleDataChange}
+                            onBlur={handleDataValidation}
+                            className={classNames({
+                                'form-control': true,
+                                'is-invalid': errors.email,
+                            })}
+                            value={data.email}
+                            placeholder="Enter email"
+                        />
+                        <div className="invalid-feedback">{errors.email}</div>
                     </div>
                     <div className="mb-3">
-                        <label className="form-label">Password</label>
-                        <div className="input-group input-group-flat">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Password"
-                                autoComplete="off"
-                                wfd-id="id2"
-                            />
-                            <span className="input-group-text">
+                        <label htmlFor="register-password" className="form-label">
+                            Password
+                        </label>
+                        <div className="row g-2">
+                            <div className="col">
+                                <input
+                                    id="register-password"
+                                    name="password"
+                                    type={revealPassword ? 'text' : 'password'}
+                                    onChange={handleDataChange}
+                                    onBlur={handleDataValidation}
+                                    className={classNames({
+                                        'form-control': true,
+                                        'is-invalid': errors.password,
+                                    })}
+                                    value={data.password}
+                                    placeholder="Enter password"
+                                    autoComplete="off"
+                                />
+                                <div className="invalid-feedback">{errors.password}</div>
+                            </div>
+                            <div className="col-auto">
                                 <a
-                                    href="#"
-                                    className="link-secondary"
+                                    onClick={handleRevealPassword}
+                                    className="btn btn-icon"
                                     data-bs-toggle="tooltip"
                                     aria-label="Show password"
                                     data-bs-original-title="Show password">
-                                    <IconEye className="icon" color="currentColor" stroke={2} size={24} />
+                                    {revealPassword ? (
+                                        <IconEyeOff className="icon" color="currentColor" stroke={2} size={24} />
+                                    ) : (
+                                        <IconEye className="icon" color="currentColor" stroke={2} size={24} />
+                                    )}
                                 </a>
-                            </span>
+                            </div>
                         </div>
                     </div>
                     <div className="mb-3">
                         <label className="form-check">
-                            <input type="checkbox" className="form-check-input" wfd-id="id3" />
+                            <input
+                                type="checkbox"
+                                value={data.terms}
+                                checked={stringToBoolean(data.terms)}
+                                onChange={() => setData({ ...data, terms: !stringToBoolean(data.terms) })}
+                                onBlur={handleDataValidation}
+                                name="terms"
+                                className="form-check-input"
+                            />
                             <span className="form-check-label">
                                 I agree to the{' '}
-                                <a href="./terms-of-service.html" tabIndex={-1}>
+                                <Link to="/not-implemented" tabIndex={-1}>
                                     terms and policy
-                                </a>
+                                </Link>
                                 .
                             </span>
                         </label>
+                        <div className="invalid-feedback d-block">{errors.terms}</div>
                     </div>
                     <div className="form-footer">
                         <button type="submit" className="btn btn-primary w-100">
