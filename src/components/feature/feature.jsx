08f79a9af9c175b5b54dc281d998df7ae4235fa9
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-13 18:52:44 +0200

    Made border wider

--- src/components/feature/feature.jsx
+++ src/components/feature/feature.jsx
@@ -4,7 +4,7 @@ import styles from './feature.module.scss';
 
 function Feature({ icon, description }) {
     return (
-        <div className={`${styles.feature} col col-3 bg-light border border-dark rounded-circle`}>
+        <div className={`${styles.feature} col col-3 bg-light border border-wide rounded-circle`}>
             {icon}
             <p className={styles['feature-description']}>{description}</p>
         </div>
