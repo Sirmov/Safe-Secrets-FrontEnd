2ab96403028f118993750d7d869b3c6d359c4704
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-16 20:36:49 +0200

    isVisible prop

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,37 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Modal({ header, children, footer, status }) {
+import { useNavigate } from 'react-router-dom';
+
+function Modal({ isVisible, status, header, children, footer }) {
+    const [show, setShow] = useState(isVisible);
+    const navigate = useNavigate();
+
+    function closeModal(event) {
+        event.preventDefault();
+
+        if (event.target.id === 'modal-overlay' || event.target.classList.contains('btn-close')) {
+            event.stopPropagation();
+            setShow(false);
+            navigate(-1);
+        }
+    }
+
     return (
-        <div
-            className="modal modal-blur fade show"
-            id="modal-danger"
-            tabIndex={-1}
-            style={{ display: 'block' }}
-            aria-modal="true"
-            role="dialog">
-            <div className="modal-dialog modal-sm modal-dialog-centered" role="document">
-                <div className="modal-content">
-                    {header !== undefined ? (
-                        <div className="modal-header">
-                            {header}
-                            <button
-                                type="button"
-                                className="btn-close"
-                                data-bs-dismiss="modal"
-                                aria-label="Close"></button>
+        <>
+            {show ? (
+                <div
+                    className="modal modal-blur fade show"
+                    id="modal-overlay"
+                    tabIndex={-1}
+                    style={{ display: 'block' }}
+                    onClick={closeModal}
+                    aria-modal="true"
+                    role="dialog">
+                    <div className="modal-dialog modal-sm modal-dialog-centered" role="document">
+                        <div className="modal-content">
+                            {header !== undefined ? (
+                                <div className="modal-header">
+                                    {header}
+                                    <button
+                                        type="button"
+                                        className="btn-close"
+                                        data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                            ) : (
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                />
+                            )}
+
+                            {status !== undefined ? <div className={`modal-status bg-${status}`} /> : null}
+
+                            <div className="modal-body text-center py-4">{children}</div>
+
+                            {footer !== undefined ? <div className="modal-footer">{footer}</div> : null}
                         </div>
-                    ) : (
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-                    )}
-
-                    {status !== undefined ? <div className={`modal-status bg-${status}`} /> : null}
-
-                    <div className="modal-body text-center py-4">{children}</div>
-
-                    {footer !== undefined ? <div className="modal-footer">{footer}</div> : null}
+                    </div>
                 </div>
-            </div>
-        </div>
+            ) : null}
+        </>
     );
 }
 
