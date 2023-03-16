e354c668660d6db91de0d977ee74017e575faf31
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-16 20:34:48 +0200

    Crud requests

--- src/services/secretsService.js
+++ src/services/secretsService.js
@@ -11,3 +11,19 @@ const endpoints = {
 export async function getAllSecrets() {
     return await httpClient.get(endpoints.allSecrets);
 }
+
+export async function getSecret(secretId) {
+    return await httpClient.get(endpoints.secret(secretId));
+}
+
+export async function createSecret(secret) {
+    return await httpClient.put(endpoints.secrets, secret);
+}
+
+export async function updateSecret(secretId, secret) {
+    return await httpClient.put(endpoints.secret(secretId), secret);
+}
+
+export async function deleteSecret(secretId) {
+    return await httpClient.delete(endpoints.secret(secretId));
+}
