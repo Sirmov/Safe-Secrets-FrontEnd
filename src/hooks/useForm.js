2d4c4171472adb45a7803dcdedff707988473680
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-19 17:02:41 +0200

    Created useForm hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+import { stringToBoolean } from '@utils/_';
+import { validateData } from '@utils/validation';
+
+function useForm(allowedData, onSubmitHandler) {
+    const [values, setValues] = useState(allowedData.reduce((acc, curr) => Object.assign(acc, { [curr]: '' }), {}));
+    const [errors, setErrors] = useState(allowedData.reduce((acc, curr) => Object.assign(acc, { [curr]: '' }), {}));
+
+    function handleChange(event) {
+        const dataKey = event.currentTarget.name;
+        const dataValue = event.currentTarget.value;
+
+        if (event.currentTarget.type === 'checkbox') {
+            setValues({ ...values, [dataKey]: !stringToBoolean(dataValue) });
+        } else {
+            setValues({ ...values, [dataKey]: dataValue });
+        }
+    }
+
+    function handleValidation(event) {
+        const dataKey = event.currentTarget.name;
+        const dataValue = event.currentTarget.value;
+        const error = validateData(dataKey, dataValue);
+
+        setErrors({ ...errors, [dataKey]: error });
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        let errors = Object.entries(values).reduce(
+            (acc, [k, v]) => Object.assign(acc, { [k]: validateData(k, v) }),
+            {}
+        );
+        let isValid = !Object.values(errors).some((e) => e.length > 0);
+
+        if (!isValid) {
+            setErrors(errors);
+        } else {
+            if (onSubmitHandler) {
+                await onSubmitHandler(event, values);
+            }
+        }
+    }
+
+    return {
+        values,
+        errors,
+        handleChange,
+        handleValidation,
+        handleSubmit,
+    };
+}
+
+export default useForm;
