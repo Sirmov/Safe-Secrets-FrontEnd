de314c28d629b784e8f6c144c982421ad74698ba
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-19 17:01:47 +0200

    Return user data

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -8,35 +8,34 @@ const endpoints = {
 
 export async function login(data) {
     const userData = await httpClient.post(endpoints.login, data);
-    // ToDo save user data
+    return userData;
 }
 
 export async function register(data) {
     const userData = await httpClient.post(endpoints.register, data);
-    // ToDo save user data
+    return userData;
 }
 
 export async function loginEmail(email, password) {
     const userData = await httpClient.post(endpoints.login, { email, password });
-    // ToDo save user data
+    return userData;
 }
 
 export async function registerEmail(email, password) {
     const userData = await httpClient.post(endpoints.register, { email, password });
-    // ToDo save user data
+    return userData;
 }
 
 export async function loginUsername(username, password) {
     const userData = await httpClient.post(endpoints.login, { username, password });
-    // ToDo save user data
+    return userData;
 }
 
 export async function registerUsername(username, password) {
     const userData = await httpClient.post(endpoints.register, { username, password });
-    // ToDo save user data
+    return userData;
 }
 
 export async function logout() {
     await httpClient.get(endpoints.logout);
-    // ToDo clear user data
 }
