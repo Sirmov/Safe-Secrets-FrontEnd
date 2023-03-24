01421d567b90629040ea21ee793704507f8c3205
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-24 08:58:06 +0200

    Storage event in fired only from different tabs

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,20 +1,6 @@
 import useStorage from '@hooks/useStorage';
 
-let isAdded = false;
-
-function handleLocalStorageChange(key, defaultValue) {
-    return function (event) {
-        if (event.key === key) {
-        }
-    };
-}
-
 function useLocalStorage(key, defaultValue) {
-    if (typeof window !== 'undefined' && !isAdded) {
-        window.addEventListener('storage', handleLocalStorageChange(key, defaultValue));
-        isAdded = true;
-    }
-
     return useStorage('localStorage', key, defaultValue);
 }
 
