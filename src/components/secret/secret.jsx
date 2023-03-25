032ce73658d5a52b051367f5332340d317daf6fd
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-25 18:37:22 +0200

    Created hide handler

--- src/components/secret/secret.jsx
+++ src/components/secret/secret.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Link } from 'react-router-dom';
 
+import SecretsContext from '@contexts/secretsContext';
+
 import styles from './secret.module.scss';
 
-function Secret({ _id, title, text, isEncrypted }) {
+function Secret({ _id, title, text, decryptedText, isEncrypted }) {
+    const { setSecrets } = useContext(SecretsContext);
+
+    function handleHide() {
+        setSecrets((secrets) =>
+            secrets.map((s) => {
+                if (s._id === _id) {
+                    s.decryptedText = '';
+                    s.isEncrypted = true;
+                }
+
+                return s;
+            })
+        );
+    }
+
     return (
         <div className={`card ${styles['secret-card']}`}>
             <div className="card-header card-header-light">
@@ -12,7 +29,7 @@ function Secret({ _id, title, text, isEncrypted }) {
             </div>
             <div className="card-body">
                 <div className="row flex-column align-items-center flex-md-row">
-                    <div className="col">{text}</div>
+                    <div className="col">{isEncrypted ? text : decryptedText}</div>
                     <div className="col-auto mt-2 mt-md-0">
                         <button className="btn btn-warning me-2">Update</button>
                         <Link to={`delete/${_id}`} className="btn btn-danger">
@@ -27,14 +44,18 @@ function Secret({ _id, title, text, isEncrypted }) {
                         <>
                             <div className="col">Secrets is encrypted.</div>
                             <div className="col-auto">
-                                <button className="btn btn-success">Decrypt</button>
+                                <Link to={`decrypt/${_id}`} className="btn btn-success">
+                                    Decrypt
+                                </Link>
                             </div>
                         </>
                     ) : (
                         <>
                             <div className="col">Secret is visible.</div>
                             <div className="col-auto">
-                                <button className="btn btn-info">Hide</button>
+                                <button className="btn btn-info" onClick={handleHide}>
+                                    Hide
+                                </button>
                             </div>
                         </>
                     )}
