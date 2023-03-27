0bfc382526d334c6c17678abf760ca4fbfb394f3
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-27 18:42:52 +0300

    Update secretDeleteModal.jsx

--- src/components/secretsList/secretDeleteModal/secretDeleteModal.jsx
+++ src/components/secretsList/secretDeleteModal/secretDeleteModal.jsx
@@ -25,7 +25,7 @@ function SecretDeleteModal() {
             })
             .catch((error) => {
                 toast.error('Something went wrong.');
-                console.log(error);
+                console.error(error);
             });
     }, [secretId]);
 
