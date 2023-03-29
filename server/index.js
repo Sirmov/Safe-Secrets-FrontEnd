ea1eb4775db11fe6816d0dde752b6acdfad4e3be
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-29 19:30:25 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,26 +1,19 @@
-try {
-    const http = require('http');
-    const createHandler = require('./src/requestHandler');
-    const services = require('./src/services');
-    const createStorage = require('./src/plugins/storage');
-    const createAuth = require('./src/plugins/auth');
-    const createUtil = require('./src/plugins/util');
-    const createRules = require('./src/plugins/rules');
+const http = require('http');
+const createHandler = require('./src/requestHandler');
+const services = require('./src/services');
+const createStorage = require('./src/plugins/storage');
+const createAuth = require('./src/plugins/auth');
+const createUtil = require('./src/plugins/util');
+const createRules = require('./src/plugins/rules');
 
-    const settings = require('./settings.json');
+const settings = require('./settings.json');
 
-    const plugins = [createStorage(settings), createAuth(settings), createUtil(settings), createRules(settings)];
+const plugins = [createStorage(settings), createAuth(settings), createUtil(settings), createRules(settings)];
 
-    const server = http.createServer(createHandler(plugins, services));
+const server = http.createServer(createHandler(plugins, services));
 
-    const port = 3030;
-    server.listen(port);
-    console.log(`Server started on port ${port}. You can make requests to http://localhost:${port}/`);
-    console.log(`Admin panel located at http://localhost:${port}/admin`);
-} catch (error) {
-    console.log('*********** Error ***********');
-    console.log(error);
-    console.log('*********** Error message ***********');
-    console.log(error.message);
-}
+const port = 3030;
+server.listen(port);
+console.log(`Server started on port ${port}. You can make requests to http://localhost:${port}/`);
+console.log(`Admin panel located at http://localhost:${port}/admin`);
 
