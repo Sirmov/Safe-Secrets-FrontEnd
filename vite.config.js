c7cefd9b58911ed1dc2f57cec1034711fe023026
Nikola Sirmov <sirmov0213@gmail.com>
2023-03-29 16:16:52 +0300

    Change output directory

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: path.resolve(__dirname, './src'),
+    build: {
+        outDir: path.resolve(__dirname, './dist'),
+    },
     envDir: __dirname,
     plugins: [react()],
     resolve: {
