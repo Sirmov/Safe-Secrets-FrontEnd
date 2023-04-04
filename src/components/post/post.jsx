edae43329909e9e9af1430f8782ade3194c2582a
Nikola Sirmov <sirmov0213@gmail.com>
2023-04-04 11:17:29 +0300

    Created post component

---
+++ src/components/post/post.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { IconThumbUp } from '@tabler/icons-react';
+import { Link } from 'react-router-dom';
+
+import { formatDateShort } from '@utils/_';
+
+function Post({ _id, title, text, _createdOn }) {
+    return (
+        <div className="card">
+            <div className="card-header">
+                <h3 className="card-title">{title}</h3>
+            </div>
+            <div className="card-body">
+                <p>{text.split(' ').slice(0, 40).join(' ')} ...</p>
+            </div>
+            <div className="card-footer">
+                <div className="row align-items-center">
+                    <div className="col">Uploaded {formatDateShort(_createdOn)}</div>
+                    <div className="col-auto">
+                        <Link to={`details/${_id}`} className="btn btn-primary me-2">
+                            Read more
+                        </Link>
+                        <IconThumbUp size={28} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Post;
---
+++ src/components/post/postSkeleton/postSkeleton.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { IconThumbUp } from '@tabler/icons-react';
+
+function PostSkeleton() {
+    return (
+        <div className="card">
+            <div className="card-header">
+                <div className="placeholder col-4" />
+            </div>
+            <div className="card-body">
+                <div className="placeholder col-10" />
+                <div className="placeholder col-10" />
+                <div className="placeholder col-10" />
+                <div className="placeholder col-10" />
+                <div className="placeholder col-10" />
+            </div>
+            <div className="card-footer">
+                <div className="row align-items-center">
+                    <div className="col">
+                        <div className="placeholder col-6" />
+                    </div>
+                    <div className="col-auto">
+                        <button className="btn btn-primary me-2">Read more</button>
+                        <IconThumbUp size={28} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PostSkeleton;
