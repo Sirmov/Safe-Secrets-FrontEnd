6e6140a220f978e971fc018f4c3d0b44b79d8ff5
Nikola Sirmov <sirmov0213@gmail.com>
2023-04-04 20:09:04 +0300

    Implemeted more methods

--- src/services/likesService.js
+++ src/services/likesService.js
@@ -4,14 +4,27 @@ const endpoints = {
     likes: '/data/likes',
     like: (likeId) => `/data/likes/${likeId}`,
     userLikes: (userId) => `/data/likes?where=_ownerId%3D%22${userId}%22`,
+    postLikes: (postId) => `/data/likes?where=_postId%3D%22${postId}%22`,
     userLike: (userId, postId) => `/data/likes?where=_ownerId%3D%22${userId}%22%20AND%20_postId%3D%22${postId}%22`,
 };
+
+export async function getAllLikes() {
+    return await httpClient.get(endpoints.likes);
+}
+
+export async function getUserLike(userId, postId) {
+    return await httpClient.get(endpoints.userLike(userId, postId));
+}
+
+export async function getPostLikes(postId) {
+    return await httpClient.get(endpoints.postLikes(postId));
+}
 
 export async function likePost(userId, postId) {
     const like = { _ownerId: userId, _postId: postId };
     return await httpClient.post(endpoints.likes, like);
 }
 
-export async function unLikePost(userId, postId) {
-    return await httpClient.delete(endpoints.userLike(userId, postId));
+export async function unLikePost(likeId) {
+    return await httpClient.delete(endpoints.like(likeId));
 }
