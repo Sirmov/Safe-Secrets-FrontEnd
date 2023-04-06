8891a1a7485ef4c78a91ad93d1b7b39282c2e12b
Nikola Sirmov <sirmov0213@gmail.com>
2023-04-06 15:55:28 +0300

    Create pageBody.test.jsx

---
+++ src/layout/pageBody/pageBody.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+
+import PageBody from './pageBody';
+
+describe('Page body layout component tests.', () => {
+    test('Page body component should render a page body container.', () => {
+        // Arrange
+        const result = render(<PageBody />);
+
+        // Assert
+        expect(result.container.querySelector('div.page-body'), 'No page body container visible.').toBeVisible();
+    });
+
+    test('Page body component should render children.', () => {
+        // Arrange
+        const text = 'I should be rendered.';
+        render(
+            <PageBody>
+                <p>{text}</p>
+            </PageBody>
+        );
+
+        // Assert
+        expect(screen.getByText(text), 'The children are not visible.').toBeVisible();
+    });
+});
