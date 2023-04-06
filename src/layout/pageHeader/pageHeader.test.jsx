90eb2b476d1297b45f5d037ad234799c59190ee5
Nikola Sirmov <sirmov0213@gmail.com>
2023-04-06 15:55:30 +0300

    Create pageHeader.test.jsx

---
+++ src/layout/pageHeader/pageHeader.test.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+
+import PageHeader from './pageHeader';
+
+describe('Page header layout component tests.', () => {
+    test('Page header component should render a page header container element.', () => {
+        // Arrange
+        const result = render(<PageHeader />);
+
+        // Assert
+        expect(
+            result.container.querySelector('div.page-header'),
+            'No page header container element visible.'
+        ).toBeVisible();
+    });
+
+    test('Page header component should render correct page title.', () => {
+        // Arrange
+        const title = 'Page title';
+        render(<PageHeader title={title} />);
+
+        // Assert
+        expect(
+            screen.getByRole('heading', { name: title }),
+            'The page title is not visible or is not correct.'
+        ).toBeVisible();
+    });
+
+    test('Page header component should render correct page subtitle.', () => {
+        // Arrange
+        const subtitle = 'Page subtitle';
+        render(<PageHeader subtitle={subtitle} />);
+
+        // Assert
+        expect(screen.getByText(subtitle), 'The page subtitle is not visible or is not correct.').toBeVisible();
+    });
+
+    test('Page header component should render children.', () => {
+        // Arrange
+        const text = 'I should be rendered.';
+        render(
+            <PageHeader>
+                <p>{text}</p>
+            </PageHeader>
+        );
+
+        // Assert
+        expect(screen.getByText(text), 'The children are not visible.').toBeVisible();
+    });
+});
