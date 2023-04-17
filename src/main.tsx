fc1fbecde10b8249f5fd3dbeac556b6e5f4acfa0
Nikola Sirmov <sirmov0213@gmail.com>
2023-04-17 18:15:43 +0300

    Migrated to typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 import ReactDOM from 'react-dom/client';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 import App from './app';
 import './main.module.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
